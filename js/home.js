56aaeb4b47fe75be871e98b6879c7dcbe257c5a6
Rimantas <belauzas@yahoo.com>
2021-08-23 11:18:51 +0300

    Git conflict prevention

--- js/home.js
+++ js/home.js
@@ -1,3 +1,30 @@
 // import JS modules
 
 // execute JS modules
+
+/* header start */
+/* header end */
+
+/* hero start */
+/* hero end */
+
+/* what we do start */
+/* what we do end */
+
+/* stop leaving start */
+/* stop leaving end */
+
+/* pricing start */
+/* pricing end */
+
+/* achievements start */
+/* achievements end */
+
+/* blog start */
+/* blog end */
+
+/* see everything start */
+/* see everything end */
+
+/* footer start */
+/* footer end */
